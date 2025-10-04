e08a29309eb95fdd733245ddb6b0393efb4eba14
mhtruong1031 <mhtruong1031@gmail.com>
2025-10-03 23:57:49 -0400

    ladiesss ladies im tryna do my work typ shi

--- YeongSil.py
+++ YeongSil.py
@@ -1,3 +1,32 @@
+from config import GEMINI_KEY
+
+from google import genai
+from google.genai import types
+
 class YeongSil:
     def __init__(self):
+        self.gemini = genai.Client()
         pass
+
+    # Takes some image and returns a description of the image from gemini and angle buckets of average depth
+    def __process_image(self, image_path: str) -> tuple[str, list[float]]:
+        with open(image_path, 'rb') as f:
+            image_bytes = f.read()
+
+        desc = self.client.models.generate_content(
+            model='gemini-2.5-flash',
+            contents=[
+                types.Part.from_bytes(
+                    data      = image_bytes,
+                    mime_type = 'image/jpeg',
+                ),
+                'Caption this image.'
+            ]
+        )
+
+        # midas model type shit
+
+    
+    def get_guidance(self, image_path: str):
+        pass
+
